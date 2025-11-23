65ecd82ac220b0e3174fdf4d567fe5237617669a
Bhoomi Chauhan <123720183+bhoomi018@users.noreply.github.com>
2025-11-23 18:17:57 +0530

    Add UpdateUserDto for user profile updates

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,18 @@
+// src/users/dto/update-user.dto.ts
+// DTO for profile updates
+
+import { IsOptional, IsString, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+}
